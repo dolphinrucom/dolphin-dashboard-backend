a79c64d9d43a38cbac191a0e9e2f69909855077b
Denis Zhitnyakov <zhitnyakovda@gmail.com>
2022-05-07 21:57:06 +0300

    Telegram

--- src/Jobs/Telegram.php
+++ src/Jobs/Telegram.php
@@ -31,48 +31,48 @@ class Telegram extends Job
     private function membersCountOfDolphinAnty()
     {
         $membersCount = $this->telegramService->membersCount('dolphin_anty');
-        $this->dataModel->insert('telegram_dolphin_anty_chat', $membersCount);
+        $this->dataModel->insert('telegram_members_dolphin_anty_chat', $membersCount);
     }
 
     private function membersCountOfDolphinAntyNews()
     {
         $membersCount = $this->telegramService->membersCount('dolphin_anty_news');
-        $this->dataModel->insert('telegram_dolphin_anty_news', $membersCount);
+        $this->dataModel->insert('telegram_members_dolphin_anty_news', $membersCount);
     }
 
     private function membersCountOfDolphin()
     {
         $membersCount = $this->telegramService->membersCount('dolphin_affiliate');
-        $this->dataModel->insert('telegram_dolphin_chat', $membersCount);
+        $this->dataModel->insert('telegram_members_dolphin_chat', $membersCount);
     }
 
     private function membersCountOfDolphinNews()
     {
         $membersCount = $this->telegramService->membersCount('dolphin_news');
-        $this->dataModel->insert('telegram_dolphin_news', $membersCount);
+        $this->dataModel->insert('telegram_members_dolphin_news', $membersCount);
     }
 
     private function membersCountOfOctoBrowser()
     {
         $membersCount = $this->telegramService->membersCount('octobrowser_chat');
-        $this->dataModel->insert('telegram_octo_browser_chat', $membersCount);
+        $this->dataModel->insert('telegram_members_octo_browser_chat', $membersCount);
     }
 
     private function membersCountOfIndigoBrowserNews()
     {
         $membersCount = $this->telegramService->membersCount('indigobrowser');
-        $this->dataModel->insert('telegram_indigo_browser_news', $membersCount);
+        $this->dataModel->insert('telegram_members_indigo_browser_news', $membersCount);
     }
 
     private function membersCountOfOctoBrowserNews()
     {
         $membersCount = $this->telegramService->membersCount('octobrowser');
-        $this->dataModel->insert('telegram_octo_browser_news', $membersCount);
+        $this->dataModel->insert('telegram_members_octo_browser_news', $membersCount);
     }
 
     private function membersCountOfFbtool()
     {
         $membersCount = $this->telegramService->membersCount('fbtoolpro');
-        $this->dataModel->insert('telegram_fbtool_chat', $membersCount);
+        $this->dataModel->insert('telegram_members_fbtool_chat', $membersCount);
     }
 }
